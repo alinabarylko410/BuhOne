d33cb3052ff0c32bba839ec9017d46ead5e412ae
Alina <80453979+alinabarylko410@users.noreply.github.com>
2021-10-29 14:00:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,70 @@
+'use strict';
+
+var header = document.querySelector('header');
+var burgermenu = document.getElementById('burger-menu');
+
+burgermenu.onclick = function() {
+	header.classList.toggle('opened');
+}
+
 $(function(){
 
-    $('.main__slider').slick({
-        dots: true, 
-        //nextArrow: "<img src='../img/arrow_next.svg' alt='next'>", 
+    $('.home__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="home-slick-btn slick-btn slick-prev"><img src="img/arrow_prev.svg" alt=""></img></button>',
+        nextArrow: '<button type="button" class="home-slick-btn slick-btn slick-next"><img src="img/arrow_next.svg" alt=""></img></button>', 
         Infinity: true,
-        autoplay: true
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll:1
     });
 
+});
+
+
+$(function(){
+    $('.clients__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="clients-slick-btn slick-btn slick-prev"><img src="img/arrow_prev.svg" alt=""></img></button>',
+        nextArrow: '<button type="button" class="clients-slick-btn slick-btn slick-next"><img src="img/arrow_next.svg" alt=""></img></button>', 
+        Infinity: true,
+        autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll:2,
+        responsive: [
+            {
+              breakpoint: 940,
+              settings: {
+                slidesToShow: 3,
+                
+              }
+            },
+            {
+                breakpoint: 740,
+                settings: {
+                  slidesToShow: 2,
+                  
+                }
+              },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                
+              }
+            }
+        ]
+    });
+});
+
+$(function(){
+    $('.reviews__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="reviews-slick-btn slick-btn slick-prev"><img src="img/arrow_prev.svg" alt=""></img></button>',
+        nextArrow: '<button type="button" class="reviews-slick-btn slick-btn slick-next"><img src="img/arrow_next.svg" alt=""></img></button>',
+        Infinity: true,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll:1
+    });
 });
